bc5748bd6ec523a8880174f9666ce4aca4133630
KogarkoP <kogarkopavel@gmail.com>
2025-08-05 21:35:33 +0300

    Thesis project started

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_DB_CONNECTION)
+  .then(console.log("Connected to DB!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port: ${process.env.PORT}`);
+});
